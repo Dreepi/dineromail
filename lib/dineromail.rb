f4ad29eb194e68c650759cbb312c1ad76a5488ae
Esteban Pastorino <ejpastorino@gmail.com>
2011-10-05 18:02:46 -0300

    module name fix on config

--- lib/dineromail.rb
+++ lib/dineromail.rb
@@ -12,7 +12,7 @@ module Dineromail
   self.configure do |config|
     #Default confiuration
     config.ipn_webservice_url = 'https://argentina.dineromail.com/Vender/Consulta_IPN.asp'
-    config.currency = Configuration::PESO_AR
+    config.currency = Currency::PESO_AR
     config.pay_methods = 'all' #Todos
     config.payment_url = 'https://argentina.dineromail.com/Shop/Shop_Ingreso.asp'
     config.button_image_url = 'https://argentina.dineromail.com/imagenes/vender/boton/comprar-gris.gif'
