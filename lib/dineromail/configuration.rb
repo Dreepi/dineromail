867f56299ce05cde3e36395c852829dbe519242c
Esteban Pastorino <ejpastorino@gmail.com>
2011-10-05 18:00:21 -0300

    typo fix

--- lib/dineromail/configuration.rb
+++ lib/dineromail/configuration.rb
@@ -2,7 +2,7 @@ module Dineromail
   class Configuration
     attr_accessor :payment_url, :ipn_webservice_url,
       :account_number, :password, :seller_name,
-      :logo_url, :button_image_url
+      :logo_url, :button_image_url,
       :ok_url, :pending_url, :error_url,
       :pay_methods, :currency, :country_id
   end
