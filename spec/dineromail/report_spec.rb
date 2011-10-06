078bad8de4a114d8b5150866fa428541796b5081
Esteban Pastorino <ejpastorino@gmail.com>
2011-10-06 19:12:22 -0300

    little fixes and added test to report0

--- spec/dineromail/report_spec.rb
+++ spec/dineromail/report_spec.rb
@@ -14,14 +14,14 @@ describe Dineromail::Report do
     report.report_status.should == 1
     report.valid_report?.should be_true
     operation.transaction_id.should == 1889
-    operation.date.should == DateTime.ordinal(2011,28,12,2,1)
-    operation.status.should == Dineromail::Operation::PENDING_STATUS
+    operation.date.should == DateTime.civil(2011, 1, 28, 12, 2, 1)
+    operation.status.should == Dineromail::OperationReport::PENDING_STATUS
     operation.pending?.should be_true
     operation.completed?.should be_false
     operation.cancelled?.should be_false
     operation.amount.should == 60.2
     operation.net_amount.should == 50.3
-    operation.pay_method.should == 1
+    operation.pay_method.should == 2
     operation.pay_medium.should == 'VISA'
     buyer.email.should == 'comprador@email.com'
     buyer.address.should == 'San Martin 10'
@@ -31,7 +31,7 @@ describe Dineromail::Report do
     buyer.document_type.should == 'DNI'
     buyer.document_number.should == '222222222'
     item.description.should == 'Libro'
-    item.currency.should == Dineromail::Currency::PESO_AR
+    item.currency.should == 1
     item.count.should == 2
     item.unit_price.should == 6.9
   end
@@ -39,6 +39,7 @@ describe Dineromail::Report do
   describe "xml request options" do
 
     let(:transaction_ids){ [42] }
+    let(:notification){ Dineromail::Notification.new }
     before do
       Dineromail.configure do |c|
         c.account_number = '10000'
@@ -47,14 +48,14 @@ describe Dineromail::Report do
     end
 
     it 'should use the configuration to get the parameters for the request if no options are given' do
-      xml_request = Dineromail::Report.xml_request_for(transaction_ids)
+      xml_request = notification.xml_request_for(transaction_ids)
       xml_request.should have_tag('nrocta', :text => '10000')
       xml_request.should have_tag('clave', :text => 'password-123')
       xml_request.should have_tag('id', :text => '42')
     end
 
     it 'should use the option parameters for the request if options are given' do
-      xml_request = Dineromail::Report.xml_request_for(transaction_ids, {:account_number => '2000',:password => 'password-456'})
+      xml_request = notification.xml_request_for(transaction_ids, {:account_number => '2000',:password => 'password-456'})
       xml_request.should have_tag('nrocta', :text => '2000')
       xml_request.should have_tag('clave', :text => 'password-456')
       xml_request.should have_tag('id', :text => '42')
